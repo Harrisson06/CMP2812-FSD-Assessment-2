f8cfe7d3c74a5f2a4ba35c09cd0408ca289dc47d
Harrisson06 <harrison.macdonald06@gmail.com>
2026-02-24 15:49:28 +0000

    get officers

--- app/API/Officers.py
+++ app/API/Officers.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.session import get_db
+from typing import list
 from app.schemas.Officers import Officers
 from app.CRUD.Officers import get_officer_by_license
+from app.CRUD.Officers import get_officers
 
 router = APIRouter()
 
@@ -10,5 +12,10 @@ router = APIRouter()
 def read_officer_by_license(drivers_license: int, db: Session = Depends(get_db)):
     officer = get_officer_by_license(db, drivers_license)
     if not officer:
+        # Raises a error code if there is no officer or license number in the database
         raise HTTPException(status_code=404, detail="No officer linked to this License")
     return officer
+
+@router.get("/Officers", response_model=list[Officers])
+def read_officers(db: Session = Depends(get_db)):
+    return get_officers
--- app/schemas/Officers.py
+++ app/schemas/Officers.py
@@ -3,9 +3,9 @@ from typing import Optional
 
 # # Base schema for the Officers table
 class OfficersBase(BaseModel):
-    First_name: Optional[str] = None
-    Last_name: Optional[str] = None
-    Perssonnel_nunber: Optional[int] = None
+    FirstName: Optional[str] = None
+    LastName: Optional[str] = None
+    PersonnelNumber: Optional[int] = None
 
 # Full schema for Officers, allows orm mapping from the db models. 
 class Officers(OfficersBase):
